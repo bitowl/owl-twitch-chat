b7a0ef0719e7ec0f7a4aab974836f4f61e740b37
bitowl <git@bitowl.de>
2018-03-24 14:31:07 +0100

    Move twitch bot to own bundle

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ owl-twitch-chat is a [NodeCG](http://github.com/nodecg/nodecg) bundle.
 It works with NodeCG versions which satisfy this [semver](https://docs.npmjs.com/getting-started/semantic-versioning) range: `~0.9.0`
 You will need to have an appropriate version of NodeCG installed to use it.
 
+It needs the [owl-twitch-bot](https://github.com/bitowl/owl-twitch-bot) bundle to be installed to fetch chat messages.
--- extension/index.js
+++ extension/index.js
@@ -1,7 +1,60 @@
 'use strict';
 
-const twitchBot = require('./twitchbot');
-
 module.exports = function (nodecg) {
-    twitchBot(nodecg);
-};
+    const twitchBot = nodecg.extensions['owl-twitch-bot'];
+    const twitchApi = nodecg.extensions['lfg-twitchapi'];
+
+    const chatMessages = nodecg.Replicant('chat-messages', {
+        defaultValue: []
+    });
+
+    twitchBot.on('message', value => {
+        fetchUserAvatar(value.username, avatar => {
+            value.avatar = avatar;
+            storeMessage(value);
+        })
+    });
+
+    function storeMessage(chatter) {
+
+        chatMessages.value.unshift(chatter); // Store the messages
+        nodecg.sendMessage('message', chatter); // Informing the dashboard of new messages
+
+        while (chatMessages.value.length > nodecg.bundleConfig.messagesCount) {
+            chatMessages.value.pop();
+        }
+
+        console.log(chatMessages.value);
+        nodecg.log.info('Received message' , chatter);
+    }
+
+    var avatarCache = {};
+
+    function fetchUserAvatar(username, callback) {
+        if (avatarCache.hasOwnProperty(username)) {
+            nodecg.log.info('Fetched avatar for ' + username + ' from cache');
+            callback(avatarCache[username]);
+            return;
+        }
+
+        twitchApi.get('/users/' + username, {
+        }).then(response => {
+            if (response.statusCode !== 200) {
+                nodecg.log.error(response.body.error, response.body.message);
+                return;
+            }
+
+            nodecg.log.info('Fetched avatar for ' + username + ' from Twitch');
+            var avatar = response.body.logo;
+            avatarCache[username] = avatar;
+            callback(avatar);
+
+        }).catch(err => {
+            nodecg.log.error(err);
+        });
+    }
+
+    nodecg.listenFor('clear-messages', () => {
+        chatMessages.value = [];
+    });
+}
