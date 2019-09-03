fb8f181ff9e177ccbc9000e5b3a35d07cf496bb5
bitowl <git@bitowl.de>
2019-09-03 23:26:14 +0200

    Add viewer list

--- dashboard/elements/owl-twitch-messages/owl-twitch-messages.js
+++ dashboard/elements/owl-twitch-messages/owl-twitch-messages.js
@@ -1,42 +1,51 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    class OwlTwitchMessages extends Polymer.Element {
-        static get is() {
-            return 'owl-twitch-messages';
-        }
+	const viewersReplicant = nodecg.Replicant('viewers', 'owl-twitch-bot');
 
-        ready() {
-            super.ready();
-            this.messages = [
-                {
-                    'username': 'system',
-                    'message': 'no messages yet loaded'
-                }
-            ];
-            nodecg.readReplicant('chat-messages', value => { // Read old messages once
-                this.messages = value;
-            });
-            nodecg.listenFor('message', value => {
-                this.unshift('messages', value);
-                if (this.messages.length > nodecg.bundleConfig.messagesCount) {
-                    this.pop('messages');
-                }
-            });
-            nodecg.listenFor('delete-message', () => {
-                this.clearMessages();
-            });
-        }
-        highlightMessage(event) {
-            nodecg.sendMessageToBundle('add-question', 'owl-question-box', event.model.item);
-        }
-        confirmClearMessages() {
-            nodecg.getDialog('delete-message').open();
-        }
-        clearMessages() {
-            this.set('messages', []);
-            nodecg.sendMessage('clear-messages')
-        }
-    }
-    customElements.define(OwlTwitchMessages.is, OwlTwitchMessages);
+	class OwlTwitchMessages extends Polymer.MutableData(Polymer.Element) {
+		static get is() {
+			return 'owl-twitch-messages';
+		}
+
+		ready() {
+			super.ready();
+			this.messages = [
+				{
+					username: 'system',
+					message: 'no messages yet loaded'
+				}
+			];
+			nodecg.readReplicant('chat-messages', value => { // Read old messages once
+				this.messages = value;
+			});
+			nodecg.listenFor('message', value => {
+				this.unshift('messages', value);
+				if (this.messages.length > nodecg.bundleConfig.messagesCount) {
+					this.pop('messages');
+				}
+			});
+			nodecg.listenFor('delete-message', () => {
+				this.clearMessages();
+			});
+			viewersReplicant.on('change', newVal => {
+				console.log('viewers', newVal);
+				this.viewers = newVal;
+			});
+		}
+
+		highlightMessage(event) {
+			nodecg.sendMessageToBundle('add-question', 'owl-question-box', event.model.item);
+		}
+
+		confirmClearMessages() {
+			nodecg.getDialog('delete-message').open();
+		}
+
+		clearMessages() {
+			this.set('messages', []);
+			nodecg.sendMessage('clear-messages');
+		}
+	}
+	customElements.define(OwlTwitchMessages.is, OwlTwitchMessages);
 })();
