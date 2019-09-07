27f14d4d1deee04a8540bd3174ab3553d9ef8b44
bitowl <git@bitowl.de>
2019-09-07 21:19:59 +0200

    Correctly fetch avatar using new Twitch api

--- extension/useravatar.js
+++ extension/useravatar.js
@@ -1,32 +1,37 @@
-'use strict';
+"use strict";
 
 var avatarCache = {};
 
 module.exports = function(nodecg) {
-    const twitchApi = nodecg.extensions['lfg-twitchapi'];
+  const twitchApi = nodecg.extensions["lfg-twitchapi"];
 
-    function fetchUserAvatar(username, callback) {
-        if (avatarCache.hasOwnProperty(username)) {
-            callback(avatarCache[username]);
-            return;
-        }
-
-        twitchApi.get('/users/' + username, {
-        }).then(response => {
-            if (response.statusCode !== 200) {
-                nodecg.log.error(response.body.error, response.body.message);
-                return;
-            }
-
-            nodecg.log.info('Fetched avatar for ' + username + ' from Twitch');
-            var avatar = response.body.logo;
-            avatarCache[username] = avatar;
-            callback(avatar);
-
-        }).catch(err => {
-            nodecg.log.error(err);
-        });
+  function fetchUserAvatar(username, callback) {
+    if (avatarCache.hasOwnProperty(username)) {
+      callback(avatarCache[username]);
+      return;
     }
 
-    return fetchUserAvatar;
-}
+    twitchApi
+      .get("/users?login=" + username, {})
+      .then(response => {
+        if (response.statusCode !== 200) {
+          nodecg.log.error(response.body.error, response.body.message);
+          return;
+        }
+
+        nodecg.log.info("Fetched avatar for " + username + " from Twitch");
+        if (response.body.data.length === 0) {
+          nodecg.log.error(`Could not fetch avatar for ${username}`);
+          return;
+        }
+        var avatar = response.body.data[0].profile_image_url;
+        avatarCache[username] = avatar;
+        callback(avatar);
+      })
+      .catch(err => {
+        nodecg.log.error(err);
+      });
+  }
+
+  return fetchUserAvatar;
+};
